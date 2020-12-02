45af3a734ebf90a435cc22c2e7300d7d2ab8f167
ta9ryuWalrus <al5.b00taku@gmail.com>
2020-12-02 22:56:00 +0900

    progress of 12/2

---
+++ steps/step12.py
@@ -0,0 +1,39 @@
+# 可変長の引数(改善編)
+
+from step09 import Variable, as_array
+import numpy as np
+
+class Function:
+    def __call__(self, *inputs):
+        xs = [x.data for x in inputs]
+        ys = self.forward(*xs)
+        if not isinstance(ys, tuple):
+            ys = (ys, )
+        outputs = [Variable(as_array(y)) for y in ys]
+
+        for output in outputs:
+            output.set_creator(self)
+        self.inputs = inputs
+        self.outputs = outputs
+
+        return outputs if len(outputs) > 1 else outputs[0]
+    
+    def forward(self, xs):
+        raise NotImplementedError()
+
+    def backward(self, gys):
+        raise NotImplementedError
+
+class Add(Function):
+    def forward(self, x0, x1):
+        y = x0 + x1
+        return y
+
+def add(x0, x1):
+    return Add()(x0, x1)
+
+if __name__ == '__main__':
+    x0 = Variable(np.array(2))
+    x1 = Variable(np.array(3))
+    y = add(x0, x1)
+    print(y.data)
